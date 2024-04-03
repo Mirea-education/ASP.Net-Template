50aa2ba67290a3bd1b6e2012d388fa2970718c45
Stas Makievsky <makievskij@mirea.ru>
2024-04-03 19:03:46 +0300

    Создание формы

---
+++ WebAssemStudent/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAssemStudent.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Create() => View();
+        
+    }
+}
